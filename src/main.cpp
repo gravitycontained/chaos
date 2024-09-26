3e268086c984c6a85635de82b5c43dbcf91c678e
DRL <d.rabl@friends-of-bytes.com>
2024-09-26 18:06:16 +0200

    updated clang format, main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
-#include "ql-core/ql.hpp"
+#include <ql/ql.hpp>
 #include <iostream>
 
 int main()
 {
-  const auto num = 5;
-  ql::println(num);
+  constexpr auto size = 5;
+  ql::constexpr_iterate<size>([](auto i) { ql::println("i : ", i); });
+
+  constexpr auto x = ql::constexpr_apply<5>([](auto i) { return i.i; });
+
+  ql::println();
 }
