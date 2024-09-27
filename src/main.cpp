fb464974acfcc08ea98703e5ede6d93c26737003
DanielRabl <danielrabl1999@gmail.com>
2024-09-27 19:32:45 +0200

    updated vcxproj and main and clang format

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,11 @@
 
 #include <iostream>
 
-template <typename T>
-  requires(ql::is_tuple<T>())
-constexpr auto tuple_to_array(T tuple)
-{
-  std::array<ql::tuple_type<0, T>, ql::tuple_size<T>()> result{};
-  auto unpack = [&]<typename Tuple, ql::size... Ints>(
-                  Tuple tuple, std::index_sequence<Ints...>)
-  {
-    ((result[Ints]
-      = ql::type_cast<ql::tuple_type<0, T>>(std::get<Ints>(tuple))),
-     ...);
-  };
-  unpack(tuple, std::make_index_sequence<ql::tuple_size<T>()>());
-  return result;
-}
-
 int main()
 {
-  constexpr auto size = 5;
-  ql::constexpr_iterate<size>([](auto i) { ql::println("i : ", i); });
+	constexpr auto x = ql::size_cast(5);
+	constexpr auto tuple = ql::to_tuple(1, 2, 3, 4);
+	constexpr auto a = ql::tuple_to_array(tuple);
 
-  constexpr auto x = ql::constexpr_apply<5>([](auto i) { return i.i; });
-
-  ql::println();
+	ql::println(a);
 }
