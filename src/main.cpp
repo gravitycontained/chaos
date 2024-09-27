bd5879e38b4b6c6fa6d151e2f9c5931fc8a02b2a
DRL <d.rabl@friends-of-bytes.com>
2024-09-27 09:40:34 +0200

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include <ql/ql.hpp>
+// #include <ql/maths/vector.hpp>
+
 #include <iostream>
+
+template <typename T>
+  requires(ql::is_tuple<T>())
+constexpr auto tuple_to_array(T tuple)
+{
+  std::array<ql::tuple_type<0, T>, ql::tuple_size<T>()> result{};
+  auto unpack = [&]<typename Tuple, ql::size... Ints>(
+                  Tuple tuple, std::index_sequence<Ints...>)
+  {
+    ((result[Ints]
+      = ql::type_cast<ql::tuple_type<0, T>>(std::get<Ints>(tuple))),
+     ...);
+  };
+  unpack(tuple, std::make_index_sequence<ql::tuple_size<T>()>());
+  return result;
+}
 
 int main()
 {
