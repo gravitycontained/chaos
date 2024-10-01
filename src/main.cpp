5afc0a9fca3801a56e48d065077ea599608ff714
DanielRabl <danielrabl1999@gmail.com>
2024-10-01 21:45:18 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <ql/ql.hpp>
+#include <ql/graphic/graphic.hpp>
 
 #include "fix-vcxproj.hpp"
 
@@ -6,18 +7,6 @@ int main()
 try
 {
 	fix_vcxproj();
-
-	using type = std::vector<std::pair<int, std::string>>;
-
-	type values = {
-			{1, "one"	 },
-			{2, "two"	 },
-			{3, "three"}
-	};
-	ql::apply_iterate(
-			values, [](auto info, auto value)
-			{ ql::println("depth: ", info.depth, ", first: ", info.first, ", last: ", info.last, ", value: ", value); }
-	);
 }
 catch (const std::exception& any)
 {
